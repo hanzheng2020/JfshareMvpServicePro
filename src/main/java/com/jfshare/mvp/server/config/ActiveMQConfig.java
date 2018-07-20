0de07834cb313a3d2939ff63e4fa53aa8aafa2a7
Adsun <fengxiang@jfshare.com>
2018-07-20 10:09:16 +0800

    加入activeMQ

---
+++ src/main/java/com/jfshare/mvp/server/config/ActiveMQConfig.java
@@ -0,0 +1,25 @@
+package com.jfshare.mvp.server.config;
+
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsMessagingTemplate;
+
+/**
+ * @author fengxiang
+ * @date 2018-07-20
+ */
+@Configuration
+public class ActiveMQConfig {
+	@Autowired
+	private JmsMessagingTemplate jmsMessagingTemplate;
+	
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("sample.queue");
+	}
+	
+}
