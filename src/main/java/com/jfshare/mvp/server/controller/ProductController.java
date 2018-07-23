e5182e78d746a1cabd304cdc370259cd34818bd3
hanzheng@jfshare.com <hanzheng@jfshare.com>
2018-07-23 17:36:01 +0800

    商品搜索

--- src/main/java/com/jfshare/mvp/server/controller/ProductController.java
+++ src/main/java/com/jfshare/mvp/server/controller/ProductController.java
@@ -1,17 +1,41 @@
 package com.jfshare.mvp.server.controller;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jfshare.mvp.server.constants.ResultConstant;
+import com.jfshare.mvp.server.model.TbProductSurvey;
+import com.jfshare.mvp.server.service.ProductService;
+import com.jfshare.mvp.server.utils.ConvertBeanToMapUtils;
+
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author fengxiang
  * @date 2018-07-23
  */
-@Api(value="api", tags="商品模块相关API")
+@Api(value = "api", tags = "商品模块相关API")
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+	@Autowired
+	private ProductService productService;
 
+	@ApiOperation(value = "根据商品id name 获取商品信息", notes = "根据商品id name 获取商品信息")
+	@GetMapping("/productSurveyQuery")
+	public ResultConstant productSurveyQuery(@RequestParam(value = "productId", required = true) String productId,
+			@RequestParam(value = "productName", required = true) String productName) {
+		List<TbProductSurvey> productList = productService.productSurveyQuery(productId, productName);
+		if(!CollectionUtils.isEmpty(productList)) {
+			return ResultConstant.ofSuccess(ConvertBeanToMapUtils.convertBeanToMap(productList));
+		}
+		return ResultConstant.ofFail(ResultConstant.FAIL_CODE_SYSTEM_ERROR, "获取商品信息失败");
+	}
 }
