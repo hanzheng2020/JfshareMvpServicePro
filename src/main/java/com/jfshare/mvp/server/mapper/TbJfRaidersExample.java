5e772f6bf45aba017cd4753217b42b57c160ba17
jfshare_xufan <xufan@officeprojp.com>
2018-07-23 13:43:37 +0800

    增加积分攻略表

--- src/main/java/com/jfshare/mvp/server/mapper/TbJfRaidersExample.java
+++ src/main/java/com/jfshare/mvp/server/mapper/TbJfRaidersExample.java
@@ -235,63 +235,63 @@ public class TbJfRaidersExample {
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeIsNull() {
-            addCriterion("create_ime is null");
+        public Criteria andCreateTimeIsNull() {
+            addCriterion("create_time is null");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeIsNotNull() {
-            addCriterion("create_ime is not null");
+        public Criteria andCreateTimeIsNotNull() {
+            addCriterion("create_time is not null");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeEqualTo(Date value) {
-            addCriterion("create_ime =", value, "createIme");
+        public Criteria andCreateTimeEqualTo(Date value) {
+            addCriterion("create_time =", value, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeNotEqualTo(Date value) {
-            addCriterion("create_ime <>", value, "createIme");
+        public Criteria andCreateTimeNotEqualTo(Date value) {
+            addCriterion("create_time <>", value, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeGreaterThan(Date value) {
-            addCriterion("create_ime >", value, "createIme");
+        public Criteria andCreateTimeGreaterThan(Date value) {
+            addCriterion("create_time >", value, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeGreaterThanOrEqualTo(Date value) {
-            addCriterion("create_ime >=", value, "createIme");
+        public Criteria andCreateTimeGreaterThanOrEqualTo(Date value) {
+            addCriterion("create_time >=", value, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeLessThan(Date value) {
-            addCriterion("create_ime <", value, "createIme");
+        public Criteria andCreateTimeLessThan(Date value) {
+            addCriterion("create_time <", value, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeLessThanOrEqualTo(Date value) {
-            addCriterion("create_ime <=", value, "createIme");
+        public Criteria andCreateTimeLessThanOrEqualTo(Date value) {
+            addCriterion("create_time <=", value, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeIn(List<Date> values) {
-            addCriterion("create_ime in", values, "createIme");
+        public Criteria andCreateTimeIn(List<Date> values) {
+            addCriterion("create_time in", values, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeNotIn(List<Date> values) {
-            addCriterion("create_ime not in", values, "createIme");
+        public Criteria andCreateTimeNotIn(List<Date> values) {
+            addCriterion("create_time not in", values, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeBetween(Date value1, Date value2) {
-            addCriterion("create_ime between", value1, value2, "createIme");
+        public Criteria andCreateTimeBetween(Date value1, Date value2) {
+            addCriterion("create_time between", value1, value2, "createTime");
             return (Criteria) this;
         }
 
-        public Criteria andCreateImeNotBetween(Date value1, Date value2) {
-            addCriterion("create_ime not between", value1, value2, "createIme");
+        public Criteria andCreateTimeNotBetween(Date value1, Date value2) {
+            addCriterion("create_time not between", value1, value2, "createTime");
             return (Criteria) this;
         }
 
