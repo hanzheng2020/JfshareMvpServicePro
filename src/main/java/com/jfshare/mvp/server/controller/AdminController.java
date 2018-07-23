7a6283768b6a4c83e797072ab5adf0adbd5cf698
Adsun <fengxiang@jfshare.com>
2018-07-23 11:09:44 +0800

    新增mapper

--- src/main/java/com/jfshare/mvp/server/controller/AdminController.java
+++ src/main/java/com/jfshare/mvp/server/controller/AdminController.java
@@ -1,10 +1,17 @@
 package com.jfshare.mvp.server.controller;
 
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jfshare.mvp.server.constants.ResultConstant;
+import com.jfshare.mvp.server.mapper.TbProductPromotion;
+import com.jfshare.mvp.server.service.ProductPromotionService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -18,10 +25,17 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/admin")
 public class AdminController {
 	
-	@ApiOperation(value="更新推广商品", 
-			notes="根据传入的推广编号，重新配置推广商品")
+	@Autowired
+	private ProductPromotionService productPromotionService;
+	
+	@ApiOperation(value="更新商品推广设置", 
+			notes="根据传入的推广配置信息，重新配置推广商品")
 	@PostMapping("/promotionProducts")
-	public ResultConstant updatePromotionProducts() {
-		return null;
+	public ResultConstant updateProductPromotion(List<TbProductPromotion> tbProductPromotions) {
+		boolean result = productPromotionService.updateProductPromotion(tbProductPromotions);
+		if (result) {
+			return ResultConstant.ofSuccess();
+		}
+		return ResultConstant.ofFail(ResultConstant.FAIL_CODE_SYSTEM_ERROR, "更新失败！");
 	}
 }
