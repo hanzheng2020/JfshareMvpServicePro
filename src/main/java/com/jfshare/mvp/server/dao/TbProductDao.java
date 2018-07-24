4e7ff0d6aaafa9086496a9125b7a47af4c318acc
hanzheng@jfshare.com <hanzheng@jfshare.com>
2018-07-24 18:22:34 +0800

    新增product增删改查

--- src/main/java/com/jfshare/mvp/server/dao/TbProductDao.java
+++ src/main/java/com/jfshare/mvp/server/dao/TbProductDao.java
@@ -11,6 +11,7 @@ import com.jfshare.mvp.server.model.ProductSurveyQueryParam;
 import com.jfshare.mvp.server.model.TbProduct;
 import com.jfshare.mvp.server.model.TbProductExample;
 import com.jfshare.mvp.server.model.TbProductSurvey;
+import com.jfshare.mvp.server.model.TbProductWithBLOBs;
 
 @Repository
 public class TbProductDao {
@@ -27,5 +28,20 @@ public class TbProductDao {
 	//根据商品名称  商品id搜索商品
 	public List<TbProductSurvey> productSurveyQuery(ProductSurveyQueryParam param){
 		return manualTbProductMapper.productSurveyQuery(param);
+	}
+	
+	//新增商品
+	public int addProduct(TbProductWithBLOBs record) {
+		return tbProductMapper.insert(record);
+	}
+	
+	//删除商品
+	public int deleteProduct(TbProductExample example) {
+		return tbProductMapper.deleteByExample(example);
+	}
+	
+	//更新商品
+	public int updateProduct(TbProduct product) {
+		return tbProductMapper.updateByPrimaryKey(product);
 	}
 }
