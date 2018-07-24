8811515e3d345a4c26d871031cfa37171c2904b3
Adsun <fengxiang@jfshare.com>
2018-07-24 09:21:38 +0800

    新增list转换

--- src/main/java/com/jfshare/mvp/server/utils/ConvertBeanToMapUtils.java
+++ src/main/java/com/jfshare/mvp/server/utils/ConvertBeanToMapUtils.java
@@ -1,7 +1,9 @@
 package com.jfshare.mvp.server.utils;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.ArrayUtils;
@@ -15,20 +17,20 @@ import org.apache.commons.lang.StringUtils;
 public class ConvertBeanToMapUtils {
 	/**
 	 * 将Java 对象转换为Map输出
-	 * @param obj 需要转换的对象
+	 * @param bean 需要转换的对象
 	 * @param excludeFields 需要排除的字段
 	 * @return
 	 */
-	public static Map<String, Object> convertBeanToMap(Object obj, String... excludeFields) {
+	public static Map<String, Object> convertBeanToMap(Object bean, String... excludeFields) {
 		Map<String, Object> map = new HashMap<String, Object>();
-		for (Field f : obj.getClass().getDeclaredFields()) {
+		for (Field f : bean.getClass().getDeclaredFields()) {
 			String fieldName = f.getName();
 			if (ArrayUtils.contains(excludeFields, fieldName) || StringUtils.equals(fieldName, "id")) {
 				continue;
 			}
 			try {
 				f.setAccessible(true);
-				map.put(fieldName, f.get(obj));
+				map.put(fieldName, f.get(bean));
 				f.setAccessible(false);
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				e.printStackTrace();
@@ -36,4 +38,13 @@ public class ConvertBeanToMapUtils {
 		}
 		return map;
 	}
+	
+	public static List<Map<String, Object>> convertBeanListToMap(List<Object> beanList, String... excludeFields) {
+		List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		for (Object bean : beanList) {
+			list.add(convertBeanToMap(bean, excludeFields));
+		}
+		return list;
+	}
+	
 }
