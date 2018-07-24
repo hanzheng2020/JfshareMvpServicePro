fa3fedd420485d1b338a3e5b91164346a71e4a01
jfshare_liujianshuai <liujianshuai@jfshare.com>
2018-07-24 09:56:22 +0800

    修改merge

--- src/main/java/com/jfshare/mvp/server/model/TbJvjindouRule.java
+++ src/main/java/com/jfshare/mvp/server/model/TbJvjindouRule.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD:src/main/java/com/jfshare/mvp/server/mapper/TbJvjindouRule.java
-package com.jfshare.mvp.server.mapper;
+package com.jfshare.mvp.server.model;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -72,80 +71,4 @@ public class TbJvjindouRule implements Serializable{
 				+ randomGivingMin + ", randomGivingMax=" + randomGivingMax
 				+ ", fixedGiving=" + fixedGiving + "]";
 	}
-=======
-package com.jfshare.mvp.server.model;
-
-import java.util.Date;
-
-public class TbJvjindouRule {
-    private Integer id;
-
-    private Date createTime;
-
-    private String givingRule;
-
-    private Integer randomGivingMin;
-
-    private Integer randomGivingMax;
-
-    private Integer fixedGiving;
-
-    private Date updateTime;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getGivingRule() {
-        return givingRule;
-    }
-
-    public void setGivingRule(String givingRule) {
-        this.givingRule = givingRule == null ? null : givingRule.trim();
-    }
-
-    public Integer getRandomGivingMin() {
-        return randomGivingMin;
-    }
-
-    public void setRandomGivingMin(Integer randomGivingMin) {
-        this.randomGivingMin = randomGivingMin;
-    }
-
-    public Integer getRandomGivingMax() {
-        return randomGivingMax;
-    }
-
-    public void setRandomGivingMax(Integer randomGivingMax) {
-        this.randomGivingMax = randomGivingMax;
-    }
-
-    public Integer getFixedGiving() {
-        return fixedGiving;
-    }
-
-    public void setFixedGiving(Integer fixedGiving) {
-        this.fixedGiving = fixedGiving;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
->>>>>>> a90299fcbc121cab5c6d7ef1fc37e5efe1b5b36c:src/main/java/com/jfshare/mvp/server/model/TbJvjindouRule.java
 }
