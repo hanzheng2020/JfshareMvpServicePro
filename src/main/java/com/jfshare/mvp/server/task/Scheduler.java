717a7e35d95f8ad1ba64b5e603d0f9a01a11de69
Adsun <fengxiang@jfshare.com>
2018-08-16 17:00:12 +0800

    新建定时器

---
+++ src/main/java/com/jfshare/mvp/server/task/Scheduler.java
@@ -0,0 +1,17 @@
+package com.jfshare.mvp.server.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author fengxiang
+ * @date 2018-06-27
+ */
+@EnableScheduling
+@Component
+public class Scheduler {
+	private final static Logger logger = LoggerFactory.getLogger(Scheduler.class);
+	
+}
