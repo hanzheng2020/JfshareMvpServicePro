c649ffd2d8d4125938e3b7a65f04ae08e818911e
Adsun <fengxiang@jfshare.com>
2018-08-21 11:23:02 +0800

    支付接口

--- src/main/java/com/jfshare/mvp/server/controller/OrderController.java
+++ src/main/java/com/jfshare/mvp/server/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.jfshare.mvp.server.controller;
 
+import java.util.Map;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,8 @@ import com.jfshare.mvp.server.service.ThirdPayService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
 
 /**
  * @author fengxiang
@@ -37,12 +42,15 @@ public class OrderController {
 	
 	@ApiOperation(value = "调用支付接口", notes = "微信支付返回prepay_id(预支付交易会话标识),支付宝返回sign(签名)")
 	@PostMapping("/thirdPay")
-	public ResultConstant thirdPay(@ApiParam(value="用户Id",required=true) String userId, 
-									@ApiParam(value="订单Id",required=true) String orderId, 
-									@ApiParam(value="订单金额",required=true) Integer orderAmount,
-									@ApiParam(value="用户端IP",required=false) String clientIp,
-									@ApiParam(value="支付方式，1代表微信，2代表支付宝",required=true) Integer payChannel) {
+	public ResultConstant thirdPay(@ApiParam(value= "{\"userId\":\"用户ID\",\"orderId\":\"订单Id\","
+									+ "\"orderAmount\":\"订单金额\",\"payChannel\":\"支付方式，1代表微信，2代表支付宝\"}")
+									@RequestBody Map<String, String> map) {
 		String result = "";
+		int payChannel = Integer.valueOf(map.get("payChannel"));
+		int orderAmount = Integer.valueOf(map.get("orderAmount"));
+		String userId = map.get("userId");
+		String orderId = map.get("orderId");
+		String clientIp = "127.0.0.1";
 		if (weChatPay == payChannel) {
 			result = thirdPayService.createWeChatPayOrder(userId, orderId, orderAmount, clientIp);
 			if (StringUtils.isEmpty(result)) {
