281a407a2d1870647f13905b9b831f759dd2de96
Adsun <fengxiang@jfshare.com>
2018-08-21 11:53:31 +0800

    支付接口

--- src/main/java/com/jfshare/mvp/server/service/ThirdPayService.java
+++ src/main/java/com/jfshare/mvp/server/service/ThirdPayService.java
@@ -1,15 +1,24 @@
 package com.jfshare.mvp.server.service;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.alipay.api.AlipayApiException;
+import com.jfshare.finagle.thrift.order.Order;
 import com.jfshare.finagle.thrift.order.OrderDetailResult;
+import com.jfshare.finagle.thrift.result.FailDesc;
+import com.jfshare.finagle.thrift.trade.BuyInfo;
 import com.jfshare.mvp.server.finagle.server.OrderClient;
 import com.jfshare.mvp.server.thirdinterface.AliPayInterface;
 import com.jfshare.mvp.server.thirdinterface.WeChatPayInterface;
+import com.jfshare.mvp.server.utils.ConstantUtil;
+import com.twitter.util.Future;
 
 /**
  * @author fengxiang
@@ -30,24 +39,41 @@ public class ThirdPayService {
 	private OrderClient orderClient;
 	
 	public boolean checkOrder(OrderDetailResult result, Integer orderAmount) {
+		Order order = result.getOrder();
+		// 校验订单是否关闭 PAY_ORDER_CLOSE
+		if ((order.getOrderState() == ConstantUtil.ORDER_STATE.TRADE_ADMIN_CANCEL.getEnumVal())
+			|| (order.getOrderState() == ConstantUtil.ORDER_STATE.TRADE_CANCEL.getEnumVal())
+			|| (order.getOrderState() == ConstantUtil.ORDER_STATE.TRADE_CLOSE.getEnumVal())
+			|| (order.getOrderState() == ConstantUtil.ORDER_STATE.WAIT_DELIVER.getEnumVal())
+			|| (order.getOrderState() == ConstantUtil.ORDER_STATE.FINISH_DELIVER.getEnumVal())
+			|| (order.getOrderState() == ConstantUtil.ORDER_STATE.WAIT_COMMENT.getEnumVal())
+			|| (order.getOrderState() == ConstantUtil.ORDER_STATE.FINISH_DELIVER.getEnumVal())) {
+			return false;
+		}
+		
+		if (orderAmount == null || !orderAmount.equals(Integer.valueOf(result.getOrder().getClosingPrice()))) {
+			return false;
+		}
 		return true;
 	}
 	
 	public String createWeChatPayOrder(String userId, String orderId, Integer orderAmount, String clientIp) {
 		OrderDetailResult result = orderClient.queryOrderDetail(userId, orderId);
 		if (checkOrder(result, orderAmount)) {
-			return weChatPayInterface.createPrepayId(result.getOrder().getProductList().get(0).getProductName(), orderId, orderAmount, clientIp);
+			return weChatPayInterface.createPrepayId("Test", orderId, orderAmount, clientIp);
 		} else {
 			return "";
 		}
 	}
 	
 	public String createAliPayOrder(String userId, String orderId, Integer orderAmount) {
-		
-		try {
-			return aliPayInterface.createPaySign();
-		} catch (AlipayApiException e) {
-			e.printStackTrace();
+		OrderDetailResult result = orderClient.queryOrderDetail(userId, orderId);
+		if (checkOrder(result, orderAmount)) {
+			try {
+				return aliPayInterface.createPaySign();
+			} catch (AlipayApiException e) {
+				e.printStackTrace();
+			}
 		}
 		return "";
 	}
