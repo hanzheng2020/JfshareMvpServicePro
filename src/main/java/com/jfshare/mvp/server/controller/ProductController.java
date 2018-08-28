f84cc19e7a008cdb78e9bb10c9d91e350025498e
hanzheng@jfshare.com <hanzheng@jfshare.com>
2018-08-28 17:31:04 +0800

    商品信息

--- src/main/java/com/jfshare/mvp/server/controller/ProductController.java
+++ src/main/java/com/jfshare/mvp/server/controller/ProductController.java
@@ -207,6 +207,9 @@ public class ProductController {
 		List<ProductCard> productCard;
 		try {
 			StringResult result = orderClient.sellerQueryDetail(sellerId,orderId);
+			if(StringUtils.isEmpty(result.getValue())) {
+				return ResultConstant.ofSuccess();
+			}
 			JSONObject jsonObj = JSONObject.parseObject(result.getValue());
 			ProductCardParam param = new ProductCardParam();
 			param.setProductId(jsonObj.getString("productId"));
