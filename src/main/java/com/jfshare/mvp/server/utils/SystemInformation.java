a1d988824ae9114c9533458995516b414bc3d20b
jfshare_xufan <xufan@officeprojp.com>
2018-09-05 14:05:20 +0800

    添加系统消息

--- src/main/java/com/jfshare/mvp/server/utils/SystemInformation.java
+++ src/main/java/com/jfshare/mvp/server/utils/SystemInformation.java
@@ -1,30 +1,26 @@
 package com.jfshare.mvp.server.utils;
 
-import java.util.HashMap;
-import java.util.Map;
 
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cn.jpush.api.JPushClient;
-import cn.jpush.api.common.ClientConfig;
+
 import cn.jpush.api.common.resp.APIConnectionException;
 import cn.jpush.api.common.resp.APIRequestException;
-import cn.jpush.api.device.TagAliasResult;
-import cn.jpush.api.device.TagListResult;
 import cn.jpush.api.push.PushResult;
 import cn.jpush.api.push.model.Platform;
 import cn.jpush.api.push.model.PushPayload;
 import cn.jpush.api.push.model.audience.Audience;
 import cn.jpush.api.push.model.notification.Notification;
-import cn.jpush.api.report.ReceivedsResult;
 
 public class SystemInformation {
 	
 	private static Logger LOG = LoggerFactory
 			.getLogger(SystemInformation.class);
 	private static String JPUSH_PUSH_MASTER_SECRET="f5e5bc9a362e7d8549dfcf75";
-	//private static String JPUSH_PUSH_APPKEY="ad0a39eae9f5b0e699bb40db";
 	private static String JPUSH_PUSH_APPKEY="aeb39fd7b8e60ac1d6a20a2a";
 	
 	
@@ -57,7 +53,7 @@ public class SystemInformation {
 	//给指定目标发送消息
 	//alias 目标别名
 	//msg  推送的消息
-	public static void  sendOnes(String alias,String msg) {
+	public static void  send(String alias,String msg) {
 		 PushPayload payload = PushPayload.newBuilder()
 		                .setPlatform(Platform.all())
 		                .setAudience(Audience.alias(alias))
@@ -72,6 +68,11 @@ public class SystemInformation {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		String mobileMd5 = DigestUtils.md5Hex("551952").toUpperCase();
+		SystemInformation.send(mobileMd5,"1433223");
 	}
 	 
 }
