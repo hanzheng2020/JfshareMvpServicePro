1d1c80a6eef0e94a07d580226f7525d91e23afa2
jfshare_xufan <xufan@officeprojp.com>
2018-09-07 17:12:44 +0800

    增加推送

---
+++ src/main/java/com/jfshare/mvp/server/service/InformationService.java
@@ -0,0 +1,23 @@
+package com.jfshare.mvp.server.service;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.stereotype.Service;
+
+import com.jfshare.mvp.server.utils.SystemInformation;
+
+@Service
+public class InformationService {
+	
+	
+	
+	public void sendMsg(String content) {
+		SystemInformation.send(content);
+	}
+	
+	
+	public void sendMsg(String userId,String title,String content,String orderId) {
+		String mobileMd5 = DigestUtils.md5Hex(userId).toUpperCase();
+		SystemInformation.buildPushObject_android_and_iosByAlias(mobileMd5,title,content,content,orderId);
+	
+	}
+}
