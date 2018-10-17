ea43cd53766ab7e62956443e2db225cde9b291ea
hanzheng@jfshare.com <hanzheng@jfshare.com>
2018-10-17 14:39:05 +0800

    商品导出问题

--- src/main/java/com/jfshare/mvp/server/service/ProductService.java
+++ src/main/java/com/jfshare/mvp/server/service/ProductService.java
@@ -177,7 +177,11 @@ public class ProductService {
 		tbProductWithBLOBs.setProductHeader(product.getProductHeader());
 		tbProductWithBLOBs.setCurPrice(product.getCurPrice());
 		tbProductWithBLOBs.setOrgPrice(product.getOrgPrice());
-		tbProductWithBLOBs.setPresentexp(product.getPresentexp());
+		if(StringUtils.isEmpty(product.getPresentexp()+"")) {
+			tbProductWithBLOBs.setPresentexp(0);
+		}else {
+			tbProductWithBLOBs.setPresentexp(product.getPresentexp());
+		}
 		tbProductWithBLOBs.setProductStock(product.getProductStock());
 		tbProductWithBLOBs.setActiveState(product.getActiveState());
 		tbProductWithBLOBs.setImgKey(product.getImgKey());
