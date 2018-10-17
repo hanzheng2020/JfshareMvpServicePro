1bd6fde80130659b3e8d3aed82328d6e9434d17f
jfshare_xufan <xufan@officeprojp.com>
2018-10-17 14:43:15 +0800

    修改积分攻略字体大小

--- src/main/java/com/jfshare/mvp/server/controller/AdminController.java
+++ src/main/java/com/jfshare/mvp/server/controller/AdminController.java
@@ -277,12 +277,13 @@ public class AdminController {
 		System.out.println("content:"+new String(jfRaider.getContent()));
 		if (!StringUtils.isEmpty(jfRaider.getContent()) && !StringUtils.isEmpty(jfRaiders.getReleaseTime())) {
 		     String content="<div style=\"text-align: center;\">\r\n" + 
-		        		"			 <div style=\"font-size:14px;>"+jfRaiders.getTitle()+"</div>\r\n" + 
+		        		"			 <div style=\'font-size:14px;'>"+jfRaiders.getTitle()+"</div>\r\n" + 
 		        		"		</div>\r\n" + 
-		        		"		<div style=\"text-align: right;color: #AAAAAA;\">"+new SimpleDateFormat("yyyy-MM-dd HH:mm").format(jfRaiders.getReleaseTime())+"</div>\r\n" + 
+		        		"		<div style=\"text-align: right;color: #AAAAAA;font-size:12px;\">"+new SimpleDateFormat("yyyy-MM-dd HH:mm").format(jfRaiders.getReleaseTime())+"</div>\r\n" + 
 		        		"		</br>"+(new String(jfRaider.getContent()));
 		        String newUrl=SendRequest.sendPost(url, "title="+jfRaiders.getTitle()+"&content="+content+"&HTMLFileName=jfgl"+jfRaiders.getReleaseTime().getTime()+".html");
 		        JSONObject obj =  JSONObject.fromObject(newUrl);
+		        System.out.println("obj>>>>>>>>>>>>>:"+obj);
 		        String code = obj.getString("code");
 		        String url=obj.getString("url");
 		        if("200".equals(code)) {
@@ -312,9 +313,9 @@ public class AdminController {
 		jfRaiders.setStatus(2);
 		jfRaiders.setReleaseTime(new Date());
         String content="<div style=\"text-align: center;\">\r\n" + 
-        		"			 <div><h2>"+jfRaiders.getTitle()+"<h2></div>\r\n" + 
+        		"			 <div style='font-size:14px;'>"+jfRaiders.getTitle()+"</div>\r\n" + 
         		"		</div>\r\n" + 
-        		"		<div style=\"text-align: right;color: #AAAAAA;\">"+new SimpleDateFormat("yyyy-MM-dd HH:mm").format(jfRaiders.getReleaseTime())+"</div>\r\n" + 
+        		"		<div style=\"text-align: right;color: #AAAAAA;style='font-size:12px;'\">"+new SimpleDateFormat("yyyy-MM-dd HH:mm").format(jfRaiders.getReleaseTime())+"</div>\r\n" + 
         		"		</br>"+(new String(jfRaiders.getContent()));
         String newUrl=SendRequest.sendPost(url, "title="+jfRaiders.getTitle()+"&content="+content+"&HTMLFileName=jfgl"+jfRaiders.getReleaseTime().getTime()+".html");
         JSONObject obj =  JSONObject.fromObject(newUrl);
